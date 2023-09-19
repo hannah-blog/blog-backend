7ffbeabc19ae4c0d344cf5f278e6a42455858bb5
Hannah <ghdcoalss33@gmail.com>
2023-09-19 18:42:07 +0900

    feat(mvc): add cors setting

---
+++ src/main/kotlin/site/hannahlog/www/global/config/mvc/MvcConfig.kt
@@ -0,0 +1,14 @@
+package site.hannahlog.www.global.config.mvc
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class MvcConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+    }
+}
